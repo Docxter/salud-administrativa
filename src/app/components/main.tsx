14d44b26c0ed6a5dad1796e1ca1ce5badd6399ff
Doc <jaime-a-c@hotmail.com>
2024-06-05 21:31:02 -0500

    Salud

--- src/app/components/main.tsx
+++ src/app/components/main.tsx
@@ -1,8 +1,10 @@
+import Image from 'next/image';
+
 export function Main () {
     return (
         <div className="p-4">
             <div className="card text-start">
-                <img className="card-img-top" src="/next.svg" alt="Title" />
+                <Image className="card-img-top" src="/next.svg" alt="Title" width={500} height={300} />
                 <div className="card-body">
                     <h4 className="card-title">Title</h4>
                     <p className="card-text">Body</p>
